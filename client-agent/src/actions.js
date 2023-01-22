bc2e0e6a671301985810a4520f43af443e86ee17
Or Levi <orlevi@Ors-MacBook-Pro.local>
2023-01-22 19:44:28 +0200

    improved the query, need to send the payload to the relevant endpoint

--- client-agent/src/actions.js
+++ client-agent/src/actions.js
@@ -24,7 +24,71 @@ const ACTIONS = {
       logger.info(`Trying to connect to ${dbConfig.database} ...`);
       await client.connect();
       logger.info(`Connected to ${dbConfig.database}`);
-      const query = `SELECT st.blk_read_time, st.blk_write_time,st.calls,st.dbid,st.local_blks_dirtied,st.local_blks_hit,st.local_blks_read,st.local_blks_written,st.max_exec_time,st.max_plan_time,st.mean_exec_time,st.mean_plan_time,st.min_exec_time,st.min_plan_time,st.plans,st.query,st.queryid,st.rows,st.shared_blks_dirtied,st.shared_blks_hit,st.shared_blks_read,st.shared_blks_written,st.stddev_exec_time,st.stddev_plan_time,st.temp_blks_read,st.temp_blks_written,st.total_exec_time,st.total_plan_time,st.userid,st.wal_bytes,st.wal_fpi,st.wal_records,d.datacl,d.datallowconn,d.datcollate,d.datconnlimit,d.datctype,d.datdba,d.datfrozenxid,d.datistemplate,d.datlastsysoid,d.datminmxid,d.datname,d.dattablespace,d.encoding,d.oid FROM pg_stat_statements as st join pg_database as d on st.dbid = d.oid order by st.calls DESC LIMIT 5000;`;
+      const query = `select 
+pgss.queryid as query_id,
+pgss.query,
+pgss.calls,
+pgss.mean_exec_time,
+pgss.dbid as db_id,
+json_build_object(
+'jit_emission_time', pgss.jit_emission_time,
+'toplevel', pgss.toplevel,
+'userid', pgss.userid,
+'plans', pgss.plans,
+'total_plan_time', pgss.total_plan_time,
+'min_plan_time', pgss.min_plan_time,
+'max_plan_time', pgss.max_plan_time,
+'mean_plan_time', pgss.mean_plan_time,
+'stddev_plan_time', pgss.stddev_plan_time,
+'total_exec_time', pgss.total_exec_time,
+'min_exec_time', pgss.min_exec_time,
+'max_exec_time', pgss.max_exec_time,
+'stddev_exec_time', pgss.stddev_exec_time,
+'rows', pgss.rows,
+'shared_blks_hit', pgss.shared_blks_hit,
+'shared_blks_read', pgss.shared_blks_read,
+'shared_blks_dirtied', pgss.shared_blks_dirtied,
+'shared_blks_written', pgss.shared_blks_written,
+'local_blks_hit', pgss.local_blks_hit,
+'local_blks_read', pgss.local_blks_read,
+'local_blks_dirtied', pgss.local_blks_dirtied,
+'local_blks_written', pgss.local_blks_written,
+'temp_blks_read', pgss.temp_blks_read,
+'temp_blks_written', pgss.temp_blks_written,
+'blk_read_time', pgss.blk_read_time,
+'blk_write_time', pgss.blk_write_time,
+'temp_blk_read_time', pgss.temp_blk_read_time,
+'temp_blk_write_time', pgss.temp_blk_write_time,
+'wal_records', pgss.wal_records,
+'wal_fpi', pgss.wal_fpi,
+'wal_bytes', pgss.wal_bytes,
+'jit_functions', pgss.jit_functions,
+'jit_generation_time', pgss.jit_generation_time,
+'jit_inlining_count', pgss.jit_inlining_count,
+'jit_inlining_time', pgss.jit_inlining_time,
+'jit_optimization_count', pgss.jit_optimization_count,
+'jit_optimization_time', pgss.jit_optimization_time,
+'jit_emission_count', pgss.jit_emission_count,
+'datacl', d.datacl,
+'datconnlimit', d.datconnlimit,
+'datfrozenxid', d.datfrozenxid,
+'datminmxid', d.datminmxid,
+'dattablespace', d.dattablespace,
+'oid', d.oid,
+'datdba', d.datdba,
+'encoding', d.encoding,
+'datlocprovider', d.datlocprovider,
+'datistemplate', d.datistemplate,
+'datallowconn', d.datallowconn,
+'datname', d.datname
+--'datcollversion', d.datcollversion,
+--'datcollate', d.datcollate,
+--'datctype', d.datctype,
+--'daticulocale', d.daticulocale
+) as metadata
+from pg_stat_statements as pgss
+join pg_database as d  on pgss.dbid = d.oid order by pgss.calls DESC LIMIT 5000;
+;`;
       const { rows } = await client.query(query);
       return rows;
     }
