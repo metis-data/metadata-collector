e45442a0fe09d0609bfe789d5dfeb775447557b1
Lior Zamir <liorz@metisdata.io>
2023-03-12 14:21:04 +0200

    revert

--- client-agent/src/metrix.js
+++ client-agent/src/metrix.js
@@ -75,7 +75,7 @@ async function run(fakeHoursDelta = 0) {
 
   logger.info('PMC Ping result', { pmcPingResult });
   // eslint-disable-next-line max-len
-  const collectingActionPromises = [collectQueries].map(
+  const collectingActionPromises = [collectQueries, collectActions].map(
     collectRunner(fakeHoursDelta, dbConfigs),
   );
   
