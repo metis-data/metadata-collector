30fed659568081e7255ebe5407c62c30ca02e6b7
Lior Zamir <liorz@metisdata.io>
2023-08-07 18:46:31 +0300

    add graceful shutdown and improve scheduler to run setup only once

--- client-agent/src/collect-runner.js
+++ client-agent/src/collect-runner.js
@@ -8,18 +8,34 @@ const { setup } = require('./setup');
 const ScheduledJob = require('./utils/scheduled-job');
 const slowQueryLogPlanCollector = require('./slow-query-log');
 
-let connections;
 
 async function app(hostedOnAws) {
   logger.info('app is staring');
   return setup()
     .then(async (_connections) => {
-      connections = _connections;
-      logger.debug('app setup has completed');
-      logger.debug('app is about to run');
-      const response = await run(0, _connections);
-      logger.debug('app has completed the running');
-      return response;
+      const scheduledJob = new ScheduledJob(async () => {
+        try {
+          return (await run(0, _connections)) || true;
+        }
+        catch (e) {
+          logger.error('scheduledJob - error: ', e);
+          return false;
+        }
+      }, ACTION_INTERVAL);
+
+      const planCollectionJob = new ScheduledJob(async () => {
+        try {
+          const results = await slowQueryLogPlanCollector(0, connections);
+          return results || true;
+        }
+        catch (e) {
+          logger.error('planCollectionJob - error: ', e);
+          return false;
+        }
+      }, SQL_PLAN_COLLECTOR_INTERVAL);
+
+      return Promise.allSettled([scheduledJob.start(), planCollectionJob.start()]);
+      
     })
     .then(() => {
       if (isDebug()) {
@@ -30,47 +46,23 @@ async function app(hostedOnAws) {
         process.exit(0);
       }
     })
-    .catch((e) => logger.error('app has failed', e));
+    .catch((e) => logger.error('error:', e));
 }
 
-async function main() {
-  try {
-    const scheduledJob = new ScheduledJob(async () => {
-      try {
-        logger.info('scheduledJob - start');
-        const results = await app();
-        logger.info('scheduledJob - end');
-        return results || true;
-      }
-      catch (e) {
-        logger.error('scheduledJob - error: ', e);
-        return false;
-      }
-    }, 1);
+// Graceful shutdown
+const gracefulShutdown = () => {
+  logger.warn('Received SIGTERM. Starting graceful shutdown...');
+  
+  // Close the PostgreSQL connection pool
+  pool.end()
+    .then(() => {
+      console.log('PostgreSQL connection pool closed.');
+      process.exit(0);
+    })
+    .catch(err => {
+      console.error('Error closing PostgreSQL connection pool:', err);
+      process.exit(1); // Exit with an error code to indicate a problem
+    });
+};
 
-    const planCollectionJob = new ScheduledJob(async () => {
-      try {
-        logger.info('planCollectionJob - start');
-        if (!connections) {
-          connections = await setup();
-        }
-        const results = await slowQueryLogPlanCollector(0, connections);
-        logger.info('planCollectionJob - end');
-        return results || true;
-      }
-      catch (e) {
-        logger.error('planCollectionJob - error: ', e);
-        return false;
-      }
-    }, SQL_PLAN_COLLECTOR_INTERVAL);
-
-    await Promise.allSettled([scheduledJob.start(), planCollectionJob.start()]);
-
-  }
-  catch (e) {
-    logger.error('error: ', e);
-  }
-}
-
-
-module.exports = main;
+module.exports = app;
